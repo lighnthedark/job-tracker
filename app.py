5661381aef6457584b50425e93cef7cca6a57f59
Spencer TGreen <spencerTgreen@yahoo.com>
2026-01-15 15:32:53 -0600

    Add dashboard with stats

--- app.py
+++ app.py
@@ -52,3 +52,8 @@ def index():
     }
     
     return render_template('index.html', jobs=jobs, stats=stats)
+
+if __name__ == '__main__':
+    import os
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
